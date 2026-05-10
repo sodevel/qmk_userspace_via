8db069f8a82cae6638d6ed9007125048415a7b00
Steffen Olszewski <steffen.o.dev@gmail.com>
2026-05-10 18:47:40 +0200

    Add YMDK YMD75 rev4 ANSI

---
+++ keyboards/ymdk/ymd75/rev4/ansi/keymaps/via/config.h
@@ -0,0 +1,6 @@
+// Copyright 2026 Steffen Olszewski (@sodevel)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+// This is required because older revisions use a different lighting system and support more layouts
+#define VIA_FIRMWARE_VERSION 1
